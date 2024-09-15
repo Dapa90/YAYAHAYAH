39cc7dbc0d8e45b44cfb923655dd56250091eb72
Dapa90 <daffamudzakir97@gmail.com>
2024-09-15 11:29:48 +0700

    buat cce

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('CEPAT SEMBUH CECECE HEHE').split('')
+    const titles = ('CEPAT SEMBUH CECE HEHE').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
