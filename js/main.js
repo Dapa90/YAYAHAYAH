66687737cef1cf0e4cbb7f9d183fd5bae6f9fe25
Dapa90 <daffamudzakir97@gmail.com>
2024-10-29 09:47:35 +0700

    wwwkkw

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('LOVE YOUUUUUU').split('')
+    const titles = ('SEMANGAT SELALU TARFA:)').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
